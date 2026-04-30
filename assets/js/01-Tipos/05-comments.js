0e9729374bebfaaf4520cc68921e4d3332d225ad
4ndr3s-00 <andresteheranr@gmail.com>
2026-04-30 13:42:35 -0500

    ....

---
+++ assets/js/01-Tipos/05-comments.js
@@ -0,0 +1,15 @@
+/*
+Todo lo que
+este dentro
+de los asteriscos
+va a ser ignorado*/
+
+/**
+ * Este es un comentario
+ * de muchas
+ * lineas, VS Code
+ */
+
+const number = 45; // Este es otro comentario
+
+console.log(number);
